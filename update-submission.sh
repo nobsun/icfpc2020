0e25afacffd9f7ecf5f04eebfb906efb8a3c12f8
Kei Hibino <ex8k.hibino@gmail.com>
2020-07-11 21:27:20 +0900

    script to update solution binary.

---
+++ update-submission.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -e
+set -x
+
+cabal v1-clean
+cabal v1-configure
+cabal v1-build -j exe:solution
+
+mkdir -p solution
+cp -a dist/build/solution/solution ./solution/
+strip ./solution/solution
