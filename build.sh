ac3ad4b94a8e1fa4e5373a96dc547c8d5f909d0b
Kei Hibino <ex8k.hibino@gmail.com>
2020-07-11 21:48:24 +0900

    update to call solution binary.

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 echo "Build not required"
+ls -l
--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ SERVER_URL=$1
 PLAYER_KEY=$2
 
 echo "ServerUrl: $SERVER_URL; PlayerKey: $PLAYER_KEY"
-curl -s "$SERVER_URL?playerKey=$PLAYER_KEY"
+./solution/solution "$SERVER_URL" "$PLAYER_KEY"
