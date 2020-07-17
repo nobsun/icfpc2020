f1cb84e5955b8e440da029ca3a78c6ffa1bdf9b9
cutsea110 <cutsea110@gmail.com>
2020-07-18 01:33:18 +0900

    draw #41's sample

--- src/Message.hs
+++ src/Message.hs
@@ -8,6 +8,8 @@ module Message (
 import Control.Applicative (empty)
 import Control.Monad.Trans.State (StateT, runStateT, get, put)
 
+import qualified Data.Tree as T
+
 -----
 
 data Node
@@ -21,6 +23,8 @@ data Node
   | S | C | B | I
   | Po2
   | Cons | Car | Cdr
+  | Nil | IsNil
+  | Apply
   deriving (Eq, Show)
 
 data Token
@@ -32,6 +36,11 @@ data Expr
   = Arg Node
   | Ap Expr Expr
   deriving (Eq, Show)
+
+cataExpr :: (Node -> t) -> (t -> t -> t) -> Expr -> t
+cataExpr f g = u
+  where u (Arg a) = f a
+        u (Ap l r) = g (u l) (u r)
 
 -----
 
@@ -68,6 +77,23 @@ toExpr = (fst <$>) . runParser (expr <* eof)
 
 -----
 
+toDataTree :: Expr -> T.Tree String
+toDataTree = cataExpr f g
+  where f a = T.Node (show a) []
+        g l r = T.Node (show Apply) [l, r]
+
+draw :: Expr -> IO ()
+draw = putStr . T.drawTree . toDataTree
+
+-----
+
 _example0 :: Maybe (Expr, [Token])
-_example0 =
-  runParser expr [TAp, TAp, TArg Sum, TArg $ Num 1, TArg $ Num 2]
+_example0 = runParser expr [TAp, TAp, TArg Sum, TArg $ Num 1, TArg $ Num 2]
+
+_drawExample41 :: IO ()
+_drawExample41 = let Just e = toExpr [ TAp, TAp, TArg B, TAp, TArg B, TAp, TAp, TArg S, TAp
+                                     , TAp, TArg B, TAp, TArg B, TAp, TArg Cons, TArg (Num 0)
+                                     , TAp, TAp, TArg C, TAp, TAp, TArg B, TArg B, TArg Cons
+                                     , TAp, TAp, TArg C, TArg Cons, TArg Nil, TAp,  TAp, TArg C
+                                     , TArg Cons,  TArg Nil, TAp, TArg C, TArg Cons]
+             in draw e
