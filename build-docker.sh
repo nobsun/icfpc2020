af35ad50f71bcdf3973e01f2ba1f6fa87f8ace0f
Masahiro Sakai <masahiro.sakai@gmail.com>
2020-07-17 12:27:36 +0900

    add build-docker.sh

---
+++ build-docker.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+mkdir -p solution
+docker run -it --name icfpc2020-build --rm --network none -v `pwd`:/icfpc2020 icfpcontest2020/haskell bash -c "cd /icfpc2020 && stack build && cp -a \`stack path --local-install-root\`/bin/solution ./solution/"
