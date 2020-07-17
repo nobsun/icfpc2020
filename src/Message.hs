3931c5cf133e43d7fdf4b22d25ce030caf167364
Kei Hibino <ex8k.hibino@gmail.com>
2020-07-18 01:25:42 +0900

    extend node data.

--- src/Message.hs
+++ src/Message.hs
@@ -13,14 +13,23 @@ import Control.Monad.Trans.State (StateT, runStateT, get, put)
 data Node
   = Num Int
   | Var Int
-  | Eq | Lt
-  | Succ | Pred
-  | Sum | Mul | Div
-  | Mod | Dem
-  | T {- True, K combinator -} | F {- False, flip K -}
-  | S | C | B | I
-  | Po2
-  | Cons | Car | Cdr
+  | Eq | Lt          -- 2 args
+  | Succ | Pred      -- 1 arg
+  | Add | Mul | Div  -- 2 arg
+  | Mod | Dem        -- 1 arg
+  | Send             -- 1 arg
+  | Neg              -- 1 arg
+  | S | C | B        -- 3 arg
+  | T {- True, K combinator -} | F {- False, flip K -}  -- 2 arg
+  | Pow2             -- 1 arg
+  | I                -- 1 arg
+  | Cons             -- 3 arg
+  | Car | Cdr        -- 1 arg
+  | Nil | IsNil      -- 1 arg
+  | If0              -- 3 arg
+  | Draw             -- 1 arg
+  | Chkb             -- 2 arg
+  | MulDraw          -- 1 arg
   deriving (Eq, Show)
 
 data Token
@@ -65,6 +74,32 @@ expr = do
 
 toExpr :: [Token] -> Maybe Expr
 toExpr = (fst <$>) . runParser (expr <* eof)
+
+-- data Value = Val Node | Fun (Value -> Value)
+
+-- FORMULA --> WHNF
+
+{-
+eval :: Expr -> Expr
+eval e@(Arg _)  = e
+eval (Ap {}) = undefined
+  where
+    eval1 e0@(Ap f e) = case eval f of
+      w@(Ap {}) -> Ap w e1
+      Arg n    -> case n of
+        Succ     -> case e1 of
+          Arg (Num i) -> Arg $ Num $ succ i
+          _           -> Ap (Arg Succ) e1
+        Pred     -> case e1 of
+          Arg (Num i) -> Arg $ Num $ pred i
+          _           -> Ap (Arg Pred) e1
+        I        -> eval e
+        Car      -> eval $ Ap e $ Arg T
+        Cdr      -> eval $ Ap e $ Arg F
+        _        -> e0
+      where
+        e1 = eval e
+ -}
 
 -----
 
