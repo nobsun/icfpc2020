de8728f42cf4803a0c6e5e9b8de9aa1cf74935d6
Kei Hibino <ex8k.hibino@gmail.com>
2020-07-18 23:57:58 +0900

    naive send implementation.

---
+++ src/Send.hs
@@ -0,0 +1,25 @@
+module Send (
+  runSend,
+  ) where
+
+import System.Process (readProcess)
+
+import Message (Prim (Cons, Nil, Num), Expr (Ap, Prim))
+import Modulate (modulate_, demodulate)
+
+runSend :: Expr -> IO Expr
+runSend e = do
+  me <- either (fail . ("runSend: request: modulate: " ++)) return $ modulate_ e
+  rbody <- readProcess
+           "curl"
+           ["-H", "accept: */*",
+            "-H", "Content-Type: text/plain",
+            "-d", me, "-s",
+            "https://icfpc2020-api.testkontur.ru/aliens/send?apiKey=a52c864b55954e25adc32abf69bc22b9"]
+           ""
+  either (fail . ("runSend: response: demodulate: " ++)) return $ demodulate rbody
+
+
+_exampleSend0 :: IO Expr
+_exampleSend0 =
+  runSend (Ap (Ap (Prim Cons) (Prim $ Num 0)) (Prim Nil))
