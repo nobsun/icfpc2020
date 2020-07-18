cec4f1dcc98f04c1a5e0e17f2b501e9fe35e2e2a
Kei Hibino <ex8k.hibino@gmail.com>
2020-07-19 03:20:59 +0900

    update to run interact galaxy.

--- src/GalaxyRun.hs
+++ src/GalaxyRun.hs
@@ -1,17 +1,29 @@
 module GalaxyRun (
-  runGalaxy,
+  runInteract,
+  interact1,
   ) where
 
 import Data.IntMap (fromList)
 import qualified Data.IntMap as IM
 
+import Message (Expr (Ap, Prim), Prim (Interact, Cons, Nil, Num))
 import Send (send)
-import Eval (Value, reduce)
+import Eval (Value, NFValue, reduce, normalize)
 import GalaxyTxt (getGalaxyExprs, galaxyKey)
 
-runGalaxy :: IO Value
-runGalaxy = do
+runInteract :: Expr -> Expr -> Expr -> IO Value
+runInteract cont vx vy = do
   ps <- getGalaxyExprs
   let envm = fromList ps
-  gexpr <- maybe (fail "galaxy expr not found!") return $ IM.lookup galaxyKey envm
-  reduce send envm gexpr
+  galaxy <- maybe (fail "galaxy expr not found!") return $ IM.lookup galaxyKey envm
+  let applyInteract = Ap (Ap (Ap (Prim Interact) galaxy) cont) (Ap (Ap (Prim Cons) vx) vy)
+  reduce send envm applyInteract
+  -- r <- reduce send envm applyInteract
+  -- normalize send envm r
+
+interact1 :: IO Value
+interact1 =
+  runInteract
+  (Prim Nil)
+  (Prim (Num 0))
+  (Prim (Num 0))
