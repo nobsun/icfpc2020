af806f1b2f79a5c319e0deaea88e13ad5fa98d50
Kei Hibino <ex8k.hibino@gmail.com>
2020-07-18 13:41:46 +0900

    add GalaxyTxt module.

---
+++ src/GalaxyTxt.hs
@@ -0,0 +1,20 @@
+module GalaxyTxt (
+  getGalaxyTxt,
+  getGalaxyTokens,
+  ) where
+
+import qualified Data.ByteString.Lazy as LB
+import qualified Data.ByteString.Lazy.Char8 as L8
+
+import Message (Token)
+import TextParser (parseLine)
+
+-- | Get content of galaxy.txt with lazy-IO
+getGalaxyTxt :: IO LB.ByteString
+getGalaxyTxt = LB.readFile "data/galaxy.txt"
+
+-- | Get tokens of galaxy.txt with lazy-IO
+getGalaxyTokens :: IO [(Int, [Message.Token])]
+getGalaxyTokens = do
+  in_ <- getGalaxyTxt
+  mapM (either fail return . parseLine) $ L8.lines in_
