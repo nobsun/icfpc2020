9b9e2081a4a0f84c0e54c6f97d5b156cf859c37e
Kei Hibino <ex8k.hibino@gmail.com>
2020-07-19 14:00:55 +0900

    just run protocol.

--- src/GalaxyRun.hs
+++ src/GalaxyRun.hs
@@ -1,29 +1,48 @@
 module GalaxyRun (
-  runInteract,
-  interact1,
+  getGalaxyProtocol,
+  runProtocol,
+  protocol1,
   ) where
 
-import Data.IntMap (fromList)
+import Data.IntMap (IntMap, fromList)
 import qualified Data.IntMap as IM
 
-import Message (Expr (Ap, Prim), Prim (Interact, Cons, Nil, Num))
-import Send (send)
-import Eval (Value, NFValue, reduce, normalize)
+import Message (Expr (Ap, Prim), Prim (Cons, Nil, Num))
+-- import Send (sendNF)
+import Eval (NFValue (..), reduceNF')
 import GalaxyTxt (getGalaxyExprs, galaxyKey)
 
-runInteract :: Expr -> Expr -> Expr -> IO Value
-runInteract cont vx vy = do
-  ps <- getGalaxyExprs
-  let envm = fromList ps
-  galaxy <- maybe (fail "galaxy expr not found!") return $ IM.lookup galaxyKey envm
-  let applyInteract = Ap (Ap (Ap (Prim Interact) galaxy) cont) (Ap (Ap (Prim Cons) vx) vy)
-  reduce send envm applyInteract
-  -- r <- reduce send envm applyInteract
-  -- normalize send envm r
 
-interact1 :: IO Value
-interact1 =
-  runInteract
-  (Prim Nil)
-  (Prim (Num 0))
-  (Prim (Num 0))
+getGalaxyProtocol :: IO (IntMap Expr, Expr)
+getGalaxyProtocol = do
+  ps <- getGalaxyExprs
+  let env = fromList ps
+  galaxy <- maybe (fail "galaxy expr not found!") return $ IM.lookup galaxyKey env
+  return (env, galaxy)
+
+runProtocol :: Expr -> (Expr, Expr) -> IO NFValue
+runProtocol state (vx, vy) = do
+  (env, galaxy) <- getGalaxyProtocol
+  let applyProtocol = Ap (Ap galaxy state) (Ap (Ap (Prim Cons) vx) vy)
+  return $ reduceNF' env applyProtocol
+
+protocol1 :: IO ()
+protocol1 = do
+  flag : newSt : dat : xs  <-
+    asList <$>
+    runProtocol
+    (Prim Nil)
+    (Prim (Num 0), Prim (Num 0))
+  putStrLn $ "flag: " ++ show (asNum flag)
+  putStrLn $ "newSt: " ++ show newSt
+  putStrLn $ "dat: " ++ show dat
+  putStrLn $ "other: " ++ show xs
+
+asNum :: NFValue -> Int
+asNum (NFPAp (Num n) []) = n
+asNum v = error $ "asNum: " ++ show v
+
+asList :: NFValue -> [NFValue]
+asList (NFPAp Nil []) = []
+asList (NFPAp Cons [x, y]) = x : asList y
+asList v = error $ "asList: " ++ show v
