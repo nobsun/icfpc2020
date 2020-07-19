fc1fed348ee2bb7e7611e6c766f20b63dcec2bd8
Kei Hibino <ex8k.hibino@gmail.com>
2020-07-19 22:47:19 +0900

    apply response decoder.

--- src/GameRun.hs
+++ src/GameRun.hs
@@ -2,26 +2,29 @@ module GameRun (
   run,
   ) where
 
-import Game (RequestTag (..), makeRequest)
+import Game (RequestTag (..), makeRequest, decodeResponse)
 import CurlCmd (gameSend)
 import Message (Expr (Prim), Prim (Nil, Num), fromList)
-import Modulate (modulate)
+import Modulate (modulate, demodulate)
 
 run :: String -> String -> IO ()
 run server playerKeyStr = do
   playerKey <- readIO playerKeyStr :: IO Int
   let request_ = request server playerKey
   joinR   <- request_ JOIN  nil
-  putStrLn $ "join response: " ++ joinR
+  either print print $ decodeResponse joinR
   startR  <- request_ START (startParam (11, 12, 13, 14))
-  putStrLn $ "start response: " ++ startR
+  either print print $ decodeResponse startR
   cmdR    <- request_ COMMANDS nil
-  putStrLn $ "commands response: " ++ cmdR
+  either print print $ decodeResponse cmdR
 
-request :: String -> Int -> RequestTag -> Expr -> IO String
-request server playerKey ptag dexpr = do
-  let reqData = modulate $ makeRequest playerKey ptag dexpr
-  gameSend server reqData
+request :: String -> Int -> RequestTag -> Expr -> IO Expr
+request server playerKey rtag dexpr = do
+  let reqData = modulate $ makeRequest playerKey rtag dexpr
+  resp  <- gameSend server reqData
+  putStrLn $ show rtag ++ " commands response: " ++ resp
+  either (fail . ("request: fail to demodulate response: " ++)) return $ demodulate resp
+  -- either fail return $ decodeResponse expr
 
 startParam :: (Int, Int, Int, Int) -> Expr
 startParam (n1, n2, n3, n4) =
