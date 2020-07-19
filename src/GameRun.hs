b4f791a0844c9b1cefb3b696de58f679278c724b
Kei Hibino <ex8k.hibino@gmail.com>
2020-07-19 23:57:10 +0900

    add debug print.

--- src/GameRun.hs
+++ src/GameRun.hs
@@ -4,24 +4,30 @@ module GameRun (
 
 import Game (RequestTag (..), makeRequest, decodeResponse)
 import CurlCmd (gameSend)
-import Message (Expr (Prim), Prim (Nil, Num), fromList)
+import Message (Expr (Prim), Prim (Nil, Num), fromList, toList)
 import Modulate (modulate, demodulate)
 
 run :: String -> String -> IO ()
 run server playerKeyStr = do
-  playerKey <- readIO playerKeyStr :: IO Int
+  playerKey <- readIO playerKeyStr
+  putStrLn $ "playerKey: " ++ show playerKey
   let request_ = request server playerKey
   joinR   <- request_ JOIN  nil
   either print print $ decodeResponse joinR
-  startR  <- request_ START (startParam (11, 12, 13, 14))
+  startR  <- request_ START (startParam (2, 3, 4, 5))
+  listPrint startR
   either print print $ decodeResponse startR
   cmdR    <- request_ COMMANDS nil
   either print print $ decodeResponse cmdR
 
+listPrint :: Expr -> IO ()
+listPrint = maybe (putStrLn "Nothing") (mapM_ print . zip [0 :: Int ..]) . toList
+
 request :: String -> Int -> RequestTag -> Expr -> IO Expr
 request server playerKey rtag dexpr = do
-  let reqData = modulate $ makeRequest playerKey rtag dexpr
-  resp  <- gameSend server reqData
+  let req = modulate $ makeRequest playerKey rtag dexpr
+  putStrLn $ show rtag ++ " command request: " ++ req
+  resp  <- gameSend server req
   putStrLn $ show rtag ++ " commands response: " ++ resp
   either (fail . ("request: fail to demodulate response: " ++)) return $ demodulate resp
   -- either fail return $ decodeResponse expr
