dd5befc9997c587876746bbb6de16ec2fa72c7c0
cutsea110 <cutsea110@gmail.com>
2020-07-19 17:22:49 +0900

    add colors around purple

--- app-play/play.hs
+++ app-play/play.hs
@@ -25,8 +25,7 @@ saveImages prefix images = do
   let ps = (0, 0):[p | pixels <- images, p <- pixels]
       min'max = minimum &&& maximum
       ((xmin, xmax), (ymin, ymax)) = min'max *** min'max $ unzip ps
-      w = xmax - xmin + 1
-      h = ymax - ymin + 1
+      (w, h) = (xmax - xmin + 1, ymax - ymin + 1)
   print ((xmin,ymin), (xmax,ymax))
   
   forM_ (zip [(0::Int)..] images) $ \(i, pixels) -> do
@@ -49,9 +48,25 @@ saveImages prefix images = do
   where
     -- TODO
     bgColor = Picture.PixelRGB8   0   0   0
-    colors = [ Picture.PixelRGB8 255   0   0
-             , Picture.PixelRGB8   0 255   0
-             , Picture.PixelRGB8   0   0 255
+    colors = [ Picture.PixelRGB8 216 164 108
+             , Picture.PixelRGB8 216 152 120
+             , Picture.PixelRGB8 220 140 140
+             , Picture.PixelRGB8 188 143 143
+             , Picture.PixelRGB8 196 124 180
+             , Picture.PixelRGB8 196 184 160
+             , Picture.PixelRGB8 240 188 168
+             , Picture.PixelRGB8 204 168 192
+             , Picture.PixelRGB8 228 136 152
+             , Picture.PixelRGB8 196  80 160
+             , Picture.PixelRGB8 168  88 168
+             , Picture.PixelRGB8 120 104 196
+             , Picture.PixelRGB8 136 120 148
+             , Picture.PixelRGB8 116 124 116
+             , Picture.PixelRGB8 120  84 124
+             , Picture.PixelRGB8 148  76  56
+             , Picture.PixelRGB8 184  72  40
+             , Picture.PixelRGB8  68  76 156
+             , Picture.PixelRGB8  96  72 120
              ]
     colorLen = length colors
 
