bf4cd692a8b1c7171cc31ac346f4633a76941696
Kei Hibino <ex8k.hibino@gmail.com>
2020-07-20 21:28:48 +0900

    solution binary. breaking take-4.

--- src/Commander/Breaking.hs
+++ src/Commander/Breaking.hs
@@ -3,7 +3,7 @@ module Commander.Breaking where
 import Game
   (Commander, StageTag (Finished), GameState (..), ShipInfo (..),
    playerRole, Command (..))
-import Vec (vneg, vsignum, (<+>))
+import Vec (vneg, vsignum)
 
 breaking :: Commander
 breaking Finished _          _      =    return []
@@ -16,11 +16,26 @@ breaking _stag    staticInfo gstate = do
       -- enemyShips = [ ship | ship <- ships, shipRole ship == enemyRole ]
 
       -- Accelerate 命令の仕様で、加速度ベクトルは逆向きに与える.
+
       -- 中心が (0,0) であることを利用して、
       -- 自分の座標位置の signum の逆と 速度の signum の和を与え、
       -- 速度低下させつつ中心から遠ざかることを狙う
+      {-
       commands =
         [ Accelerate (shipId ship) ( vneg (vsignum $ shipPos ship) <+> vsignum (shipVel ship) )
         | ship <- myShips ]
+       -}
 
+      -- 中心が (0,0) であることを利用して、
+      -- 自分の座標位置の signum の逆を与え、
+      -- 中心から遠ざかることを狙う
+      commands_ =
+        [ (Accelerate (shipId ship) (vneg $ vsignum $ shipPos ship), shipPos ship)
+        | ship <- myShips ]
+
+      commands = map fst commands_
+
+      dprint (cmd, pos) = putStrLn $ "breaking: " ++ show cmd ++ " : " ++ show pos
+
+  mapM_ dprint commands_
   return commands
