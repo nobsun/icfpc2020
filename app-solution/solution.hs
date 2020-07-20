e1bb5c8346df5e086bd7172fe3cc47de5c9c525e
Kei Hibino <ex8k.hibino@gmail.com>
2020-07-20 20:27:34 +0900

    shooter solution binary.

--- app-solution/solution.hs
+++ app-solution/solution.hs
@@ -2,9 +2,9 @@
 import System.Environment (getArgs)
 
 import GameRun (run)
-import Commander.Further (further)
+import Commander.Shooter (shooter)
 
 main :: IO ()
 main = do
   args <- getArgs
-  run further (args !! 0) (args !! 1)
+  run shooter (args !! 0) (args !! 1)
